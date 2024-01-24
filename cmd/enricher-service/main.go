5943052b4322b9a1860d842dd4e08ed6770abe90
Pavel Timokhovich <t1m0kh0v14@gmail.com>
2024-01-24 22:38:52 +0300

    update main: setup logger

--- cmd/enricher-service/main.go
+++ cmd/enricher-service/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"github.com/binaryty/enricher-service/internal/config"
+	"log/slog"
+	"os"
+)
+
 const (
 	envLocal = "local"
 	envDev   = "dev"
@@ -7,13 +13,32 @@ const (
 )
 
 func main() {
-	// TODO: init config
+	// init config
+	cfg := config.MustLoad()
 
-	// TODO: init logger
+	// init logger
+	logger := setupLogger(cfg.Env)
+	logger.Info("start logging")
 
 	// TODO: init storage
 
 	// TODO: init router
 
 	// TODO: run server
+}
+
+// setupLogger ...
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
